b1af8e88fef975437046c8a6de7aa8dfc20102d4
Ujwal-Hiranwar <ujjwalhiranwar15@gmail.com>
2025-10-19 10:50:03 +0000

    final version pushed

--- Backend/application/database-setup.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE quiz_application;
-CREATE USER 'quiz_user'@'localhost' IDENTIFIED BY 'quiz_password123';
-GRANT ALL PRIVILEGES ON quiz_application.* TO 'quiz_user'@'localhost';
-FLUSH PRIVILEGES;
